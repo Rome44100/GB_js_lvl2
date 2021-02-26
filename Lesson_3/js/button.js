270dfe58a62a374df725aae9229c7c3578180947
Rome44100 <rome44100@gmail.com>
2021-02-26 15:02:53 +0300

    Complete Lesson 3

--- Lesson_3/js/button.js
+++ Lesson_3/js/button.js
@@ -3,12 +3,15 @@
 class Button {
     _text = "";
     _callback = null;
+    _id = null;
+    _placeToRender = null;
 
     constructor(text, callback) {
         this._text = text;
         this._callback = callback;
+        this._id = Date.now() + Math.round(Math.random() * 100000); // можно hash добавить, random и пр.
         //this.render();
-    }
+    };
 
     onBtnClick() {
         //console.log("Was clicked!");
@@ -16,18 +19,20 @@ class Button {
         if (typeof callback === "function") {
             callback();
         }
-    }
+    };
 
     getTemplate() {
         const btn = document.createElement("button");
         btn.classList.add("btn");
+        btn.id = this._id;
 
         return btn;
-    }
+    };
 
     render(placeToRender) {
         //const placeToRender = document.querySelector(".btns");
         if (placeToRender) {
+            this._placeToRender = placeToRender;
             const btn = this.getTemplate();
             btn.innerHTML = this._text;
             placeToRender.appendChild(btn);
@@ -36,6 +41,11 @@ class Button {
                 this.onBtnClick();
             });
         }
+    };
+
+    destroy() {
+        const el = this._placeToRender.querySelector(`[id = "${this._id}"]`);
+        el.parentNode.removeChild(el);
     }
 }
 
--- Lesson_3/js/shop.js
+++ Lesson_3/js/shop.js
@@ -6,38 +6,62 @@ class AbstractList {
 
 class List extends AbstractList {
 
+    _elemsAddCnt = 1;
+    _url = "http://localhost:3000/database/items.json";
+
     constructor(CartInstance) {
         // при переопределении конструктора родителя нужно обязательно вызывать super
         // при этом если нет аргументов у родителя, то можно и без них в дочернем вызывать super
         super();
 
-        this.fetchGoods()
+        this.addElems(this._url);
+    };
+
+    fetchGoods() {
+        // fetch возвращает promise
+        return fetch(this._url);
+    };
+
+    render() {
+        this._items.forEach(Elem => {
+            Elem.render();
+        });
+
+        const placeToRender = document.querySelector(".products_wrap");
+        this._url = `http://localhost:3000/database/items${this._elemsAddCnt}.json`;
+
+        if (placeToRender) {
+            const btn = new Button("Еще товары", () => {
+                this.addElems(this._url, btn);
+                this._elemsAddCnt++;
+            });
+            if (this._elemsAddCnt < 3) {
+                btn.render(placeToRender);
+            }
+        }
+    };
+
+    // в этот раз не побоялся и вытащил в отдельный метод и не прогадал :)
+    // но при этом остается за гранью понимания, как CartInstance работает во всех этих вложенных областях видимости
+    addElems(url, btn = null) {
+        fetch(url)
             .then(res => {
-                // сначала распарсить
                 return res.json();
-                // возвращает всё равно promise
             })
             .then(obj => {
-                // дописать свойство data у obj, т.к. в файле items.json сначала всё завернуто в свойство data
                 const goods = obj.data.map(el => {
                     return new GoodsItem(el, CartInstance);
                 });
                 this._items = goods;
                 return this._items;
             })
+            .then(() => {
+                if (null != btn) {
+                    btn.destroy();
+                }
+            })
             .then(this.render.bind(this));
-    }
-
-    fetchGoods() {
-        // fetch возвращает promise
-        return fetch("http://localhost:3000/database/items.json");
-    }
-
-    render() {
-        this._items.forEach(Elem => {
-            Elem.render();
-        });
-    }
+    };
 }
 
 class GoodsItem {
@@ -146,7 +170,7 @@ class Cart extends AbstractList {
         });
 
         this._items = goods;
-    }
+    };
 
     render() {
         let place = document.querySelector(".basket_list");
@@ -222,8 +246,6 @@ class Cart extends AbstractList {
                     arr.splice(index, 1);
                     this._names.splice(index, 1);
                     event.target.parentNode.parentNode.remove();
-
-                    console.log(this);
                 });
             });
         }
@@ -235,7 +257,7 @@ class Cart extends AbstractList {
 
         total_cnt.innerHTML = this._total_cnt;
         basket_price.innerHTML = this._basket_price;
-    }
+    };
 }
 
 class CartItem {
