2ff4beb11e3f2713379d8ded7dc6bb1997f47e0b
Rome44100 <rome44100@gmail.com>
2021-02-28 15:10:14 +0300

    Start Lesson 4

---
+++ Lesson_4/js/main.js
@@ -0,0 +1,32 @@
+"use strict";
+// будем показывать только если была прокрутка на одно окон вниз
+window.addEventListener('scroll', function () {
+    if (pageYOffset < document.documentElement.clientHeight) {
+        document.querySelector("#triton_up").style.opacity = "0";
+        document.querySelector("#triton_up").style.visibility = "hidden";
+    } else {
+        document.querySelector("#triton_up").style.opacity = ".4";
+        document.querySelector("#triton_up").style.visibility = "visible";
+    }
+});
+// сделаем плавную прокрутку
+document.getElementById("triton_up").addEventListener('click', function (e) {
+    e.preventDefault();
+    document.getElementById("top").scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+});
+
+class FormValidate {
+    _checked = false;
+    _form = null;
+
+    constructor(form) {
+        this._form = form;
+    }
+
+    validate() {
+
+    }
+}
