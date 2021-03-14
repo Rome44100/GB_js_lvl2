bc5cd99c62f4fe15dbfaad21ad30a38c992a6923
Rome44100 <rome44100@gmail.com>
2021-03-14 13:53:33 +0300

    Start Lesson 7

---
+++ Lesson_7/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <Catalog />
+  </div>
+</template>
+
+<script>
+import Catalog from "./components/Catalog.vue";
+
+import Cart from "./components/Cart.vue";
+
+export default {
+  components: {
+    Catalog,
+    Cart,
+  },
+};
+</script>
+
+<style module>
+</style>
