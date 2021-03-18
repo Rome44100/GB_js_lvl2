f3da3eb095246515d8ded1dce6216b31236451ad
Rome44100 <rome44100@gmail.com>
2021-03-18 17:46:33 +0300

    Start Lesson 8

---
+++ Lesson_8/src/components/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="$style.basket_vue">
+    Корзина пуста
+    <CartItem v-for="id in getItemsInCart" :key="id" :id="id" />
+    <div>
+      <hr />
+      Общая стоимость всей корзины:
+      <span id="basket_price">{{ getFullPrice }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import CartItem from "./CartItem.vue";
+
+import { mapMutations, mapGetters, mapActions } from "vuex";
+
+export default {
+  methods: {},
+  computed: {
+    ...mapGetters(["getItemsInCart", "getFullPrice"]),
+  },
+  components: {
+    CartItem,
+  },
+};
+</script>
+
+<style module>
+.basket_vue {
+  margin: 0 0 40px;
+}
+</style>
